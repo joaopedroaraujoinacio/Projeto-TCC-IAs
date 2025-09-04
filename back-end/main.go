47ead02199f9203e14daeb713261ed097d399704
etienne pautet <etienne.pautet@gmail.com>
2025-09-04 20:48:12 -0300

    fix/removed unused docs/swagger imports

--- back-end/main.go
+++ back-end/main.go
@@ -1,30 +1,15 @@
-// @title           Your API Title
-// @version         1.0
-// @description     This is your API description
-// @termsOfService  http://swagger.io/terms/
-
-// @contact.name   API Support
-// @contact.url    http://www.swagger.io/support
-// @contact.email  support@swagger.io
-
-// @license.name  Apache 2.0
-// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host      localhost:8080
-// @BasePath  /api/v1
-
 package main
 
 import (
-	"log"
 	"os"
-	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
+	"log"
+	// _"golang_crud/docs"
 	"golang_crud/config"
-	"golang_crud/database"
 	"golang_crud/routes"
-	_"golang_crud/docs"
+	"golang_crud/database"
+	"github.com/gin-gonic/gin"
+	// swaggerFiles "github.com/swaggo/files"
+	// ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
@@ -46,7 +31,7 @@ func main() {
 		port = "8080"
 	}
 
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	// r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	log.Printf("Server starting on port %s", port)
 	r.Run("0.0.0.0:" + port)
