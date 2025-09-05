c7800fd9611165e2905af8002c9a6f2e2085c07b
Etienne <etienne.pautet@gmail.com>
2025-09-05 11:19:50 -0300

    feat/image embeding, added init.sql for image model

--- back-end/init.sql
+++ back-end/init.sql
@@ -1,5 +1,20 @@
 CREATE EXTENSION IF NOT EXISTS vector;
 
+CREATE TABLE IF NOT EXISTS image_embeddings(
+    id SERIAL PRIMARY KEY,
+    file_name VARCHAR(255) NOT NULL,
+    file_path VARCHAR(500) NOT NULL,
+    description TEXT,
+    embedding vector(4096),
+    metadata JSONB,
+    created_at TIMESTAMP DEFAULT TIMESTAMP
+),
+
+CREATE INDEX IF NOT EXISTS image_embedding_idx 
+ON image_embeddings USING ivfflat (embedding vector_cosine_ops)
+WITH (lists = 1);
+
+
 CREATE TABLE IF NOT EXISTS documents (
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
@@ -33,6 +48,7 @@ BEGIN
     LIMIT match_count;
 END;
 $$ LANGUAGE plpgsql;
+
 
 CREATE TABLE IF NOT EXISTS codes (
     id SERIAL PRIMARY KEY,
